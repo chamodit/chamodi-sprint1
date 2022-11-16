6ffc2c09922cf330d96cc90f933821e0c12fffc4
IT18231724 <58078417+chamodit@users.noreply.github.com>
2022-11-16 23:51:57 +0530

    Overview Component

--- dashboard-yta/src/app/app-routing.module.ts
+++ dashboard-yta/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './components/overview/overview.component';
+import { AllProjectsComponent } from './components/all-projects/all-projects.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { TeamsComponent } from './components/teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview', component: OverviewComponent },
+  { path: 'all-projects', component: AllProjectsComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: 'teams', component: TeamsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
